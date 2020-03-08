7669b99c32e2f8e899a35b8091d9448072ec47bc
Elin Falla <elinfalla@me.com>
2020-03-08 08:51:27 -0700

    functioning experience page

--- src/Parent.js
+++ src/Parent.js
@@ -4,32 +4,39 @@ import React from 'react';
 // import Education from './Education.js';
 import Education from './Components/Education/Education.js';
 // import Skills from './Components/Skills/Skills.js';
-// import Experience from './Components/Experience/Education.js';
+import Experience from './Components/Experience/Experience.js';
 // import Projects from './Components/Projects/Education.js';
 // import Extracurriculars from './Components/Extracurriculars/Extracurriculars.js';
 import { Stepper, Step, StepLabel, Button } from '@material-ui/core';
+import TechExp from './Components/TechExp/TechExp.js'
+import OtherExp from './Components/OtherExp/OtherExp.js'
 
 class Parent extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            activeStep: 0,
+            activeStep: 2,
             steps: ['Education', 'Skills', 'Experience', 'Projects', 'Extracurriculars'],
             educationChanges: {},
+            TechExp: [<TechExp key={1}/>],
+            OtherExp: [<OtherExp key={1}/>],
+
         }
         this.handlePrev = this.handlePrev.bind(this);
         this.handleNext = this.handleNext.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.addTechExp = this.addTechExp.bind(this);
+        this.addOtherExp = this.addOtherExp.bind(this);
     }
 
-    async handlePrev(index) {
-        const activeStep = index--;
+    async handlePrev() {
+        const activeStep = this.state.activeStep- 1;
         await this.setState({ ...this.state, activeStep });
     }
 
-    async handleNext(index) {
-        const activeStep = index++;
+    async handleNext() {
+        const activeStep = this.state.activeStep + 1;
         await this.setState({ ...this.state, activeStep });
     }
 
@@ -43,6 +50,30 @@ class Parent extends React.Component {
         }
     }
 
+    addTechExp() {
+            this.setState(
+                {
+                    ...this.state,
+                    TechExp: [
+                        ...this.state.TechExp,
+                        <TechExp key={this.state.TechExp.length + 1}/>
+                    ]
+                }
+            );
+        }
+
+        addOtherExp() {
+                this.setState(
+                    {
+                        ...this.state,
+                        OtherExp: [
+                            ...this.state.OtherExp,
+                            <OtherExp key={this.state.OtherExp.length + 1}/>
+                        ]
+                    }
+                );
+            }
+
     render() {
         const { state } = this;
         return <React.Fragment>
@@ -55,13 +86,18 @@ class Parent extends React.Component {
             </Stepper>
             <Button disabled={state.activeStep < 1} onClick={this.handlePrev}>Prev</Button>
             <Button disabled={state.activeStep > 3} onClick={this.handleNext}>Next</Button>
-            {(state.activeStep === 0) && 
+            {(state.activeStep === 0) &&
                 <Education
                     handleChange={this.handleChange}
                 />}
-            {/* {(state.activeStep === 1) && <Skills />}
-            {(state.activeStep === 2) && <Experience />}
-            {(state.activeStep === 3) && <Projects />}
+            {/*{(state.activeStep === 1) && <Skills />*/}
+            {(state.activeStep === 2) && <Experience
+              TechExp={this.state.TechExp}
+              addTechExp={this.addTechExp}
+              OtherExp={this.state.OtherExp}
+              addOtherExp={this.addOtherExp}
+            />}
+            {/*(state.activeStep === 3) && <Projects />}
             {(state.activeStep === 4) && <Extracurriculars />} */}
         </React.Fragment>
     }
